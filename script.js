73cdf505f2f5b0c4bcebc5bb02c68c123b85997c
eric zhou <eric.z.zhou@yamibuy.com>
2022-08-06 13:21:50 +0800

    完善页面内容渲染

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ class LazyHtml {
     this.options = options;
   }
 
+  /**
+   * 分发不同处理类型
+   * @param {*} config
+   * @param {*} element
+   */
+  async dispatch(config, element) {
+    let { method, response } = config;
+    let result = null; // 初始化返回结果
+    switch (method) {
+      case "ajax":
+        result = await new AjaxHandler(element, config).init();
+        break;
+      default:
+        console.info("没有匹配到正确的分发器", config);
+        break;
+    }
+
+    return result;
+  }
+
   //监听页面滚动
   listenScroll(callback) {
     window.addEventListener("scroll", callback);
@@ -58,8 +78,24 @@ class LazyHtml {
         offsetTop + offsetHeight + distance > scrollTop &&
         offsetTop < scrollTop + clientHeight + distance
       ) {
-        this.setLazyAttr(element, "loaded");
-        this.options.onLoad();
+        try {
+          let jsonStr = element.querySelector(
+            'script[type="application/json"]'
+          ).textContent;
+          console.log("json 配置内容", jsonStr);
+          let config = JSON.parse(jsonStr);
+
+          this.dispatch(config, element).then((res) => {
+            this.setLazyAttr(element, "loaded");
+            this.options.onLoad(element, config, this.options);
+          });
+        } catch (e) {
+          console.error(
+            `没有发现元素配置，请检查元素内是否有 <script type="application/json"> 标签，并且内容是 JSON 格式`,
+            element,
+            e
+          );
+        }
       }
     });
   }
@@ -87,5 +123,56 @@ class LazyHtml {
         fn();
       }, 500);
     };
+  }
+}
+
+class AjaxHandler {
+  constructor(element, config) {
+    this.element = element;
+
+    this.config = config;
+    console.log(element, config);
+  }
+
+  /**
+   *
+   * @returns {Object} 返回对象 { type , content }
+   */
+  async init() {
+    // https://www.ruanyifeng.com/blog/2020/12/fetch-tutorial.html
+    let fetchConfig = {
+      method: this.config.response.method,
+      headers: this.config.response.headers,
+    };
+
+    if (this.config.response.method === "POST") {
+      fetchConfig.body = this.config.response.body;
+    }
+    let response = await fetch(this.config.response.url, fetchConfig);
+    let result;
+    if (this.config.response.type === "html") {
+      result = await response.text();
+      this.DrawHtml(result);
+    } else {
+      result = await response.json();
+    }
+    return {
+      type: this.config.response.responseType,
+      content: result,
+    };
+  }
+
+  DrawHtml(html) {
+    if (!this.element) return;
+
+    if (
+      this.config &&
+      this.config.target &&
+      this.element.querySelector(this.config.target)
+    ) {
+      this.element.querySelector(this.config.target).innerHTML = html;
+    } else {
+      this.element.innerHTML = html;
+    }
   }
 }
